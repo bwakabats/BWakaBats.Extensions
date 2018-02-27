8bb312eb48682ed40265eaac21d3808780f45c9d
Shane Whitehead <shane.whitehead@werewebsoftware.co.uk>
2018-02-27 08:17:12 +0000

    Change API key

--- nugetPublish.bat
+++
@@ -1,3 +0,0 @@
-nuget pack BWakaBats.Extensions.csproj -Prop Configuration=Release
-nuget push BWakaBats.Extensions.1.0.2.nupkg 5b761e29-f5c9-4a38-bce3-75241deca054 -Source https://www.nuget.org/api/v2/package
-
